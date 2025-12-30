0e27387b2d52219ecb65d4f024f131864d4bfacb
Tejaswini Turlapati <tejteja097@gmail.com>
2025-12-30 18:59:09 +0530

    add opd service to remote

---
+++ src/main/java/com/healthify/opdservice/OpdserviceApplication.java
@@ -0,0 +1,13 @@
+package com.healthify.opdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthify/opdservice/OpdserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthify.opdservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpdserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
